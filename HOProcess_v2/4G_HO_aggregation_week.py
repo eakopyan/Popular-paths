c51b71286099f605a1844e2f7e66feca319a50e0
Evelyne Akopyan <evelyne.akopyan@gmail.com>
2025-06-12 15:49:27 +0200

    playing with data

--- HOProcess_v2/4G_HO_aggregation_week.py
+++ HOProcess_v2/4G_HO_aggregation_week.py
@@ -12,7 +12,7 @@ def get_filename(date, folder):
     return filename
 
 
-FOLDER = 'CSV_ho'
+FOLDER = 'CSV_ho'  # 'cells', 'CSV_ho' or 'CSV_nbimsi'
 
 DATES = ['2024-10-28',
          '2024-10-29',
@@ -21,14 +21,14 @@ DATES = ['2024-10-28',
          '2024-11-01',
          '2024-11-02',
          '2024-11-03']
-EXPORT_PATH = 'Processed_data\\4G_'+FOLDER+'_AGG_week.snappy.parquet'
+EXPORT_PATH = 'Processed_data\\4G_'+FOLDER+'_AGG_2024-10-28.snappy.parquet'
 
 print('Aggregating '+FOLDER+' data from '+DATES[0]+' to '+DATES[-1])
 
 week_list = []
 
-for day in DATES:
-#for day in ['2024-10-28']:
+#for day in DATES:
+for day in ['2024-10-28']:
     print('\tParsing '+day+'...')
     filename = get_filename(day, folder=FOLDER)
     df = pd.read_csv(filename)
@@ -38,12 +38,17 @@ print('Starting aggregation...')
 frame = pd.concat(week_list, axis=0, ignore_index=True)
 
 if FOLDER=='CSV_ho':
-    df_agg = pd.DataFrame(frame.groupby(['prev_eci_4g', 'eci_4g'])['nb_ho'].sum())
+    df_agg = pd.DataFrame(frame.groupby(['prev_eci_4g', 'eci_4g'])[['nb_imsi','nb_ho']].sum())
     df_agg = df_agg[df_agg['nb_ho'] >0] 
     df_agg = df_agg.reset_index()
     df_agg = df_agg.astype(int)
 elif FOLDER=='cells':
     df_agg = frame.drop_duplicates(ignore_index=True)
+    cells_list = []
+    for id, row in df_agg.iterrows():
+        prev_name = row['nom_cellule'].split('_')
+        cells_list.append('_'.join(prev_name[0:-2]))
+    df_agg['Cell'] = cells_list
 
 print('\n... Done. \nAggregated '+FOLDER+' DataFrame:')
 print(df_agg)
